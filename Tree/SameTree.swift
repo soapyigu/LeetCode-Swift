89ab67961c65f7c65e7f1e71ed750c548e300f4e
Yi Gu <yi@superfamily.me>
2016-03-30 23:04:22 -0700

    [Tree] add Solution to SameTree

---
+++ Tree/SameTree.swift
@@ -0,0 +1,32 @@
+/**
+ * Question Link: https://leetcode.com/problems/same-tree
+ * Primary solution: recursion
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ * Copyright © 2016 YiGu. All rights reserved.
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class SameTree {
+    func isSameTree(p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if (p == nil && q == nil) {
+            return true
+        }
+        if (p == nil || q == nil || p?.val != q?.val) {
+            return false
+        }
+    
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+}

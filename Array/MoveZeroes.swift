e2f9ef378550d0f3fd1eb2233e3efaa6d73dfe4d
Yi Gu <yi@superfamily.me>
2016-04-09 13:39:12 -0700

    [Array] add solution to Move Zeros and Remove Element

---
+++ Array/MoveZeroes.swift
@@ -0,0 +1,29 @@
+/**
+ * Question Link: https://leetcode.com/problems/move-zeroes/
+ * Primary idea: keep index zeroIndex, traverse through the array and swap the value
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ */
+
+class MoveZeroes {
+    func moveZeroes(inout nums: [Int]) {
+        var zeroIndex: Int = 0
+        var left: Int = 0
+        let right: Int = nums.count
+        
+        while left < right {
+            if nums[left] != 0 {
+                _swap(&nums, zeroIndex, left)
+                zeroIndex += 1
+            }
+            left += 1
+        }
+    }
+    
+    private func _swap(inout nums: [Int], _ p: Int, _ q: Int) {
+        let temp: Int = nums[p]
+        nums[p] = nums[q]
+        nums[q] = temp
+    }
+}
---
+++ Array/RemoveElement.swift
@@ -0,0 +1,25 @@
+/**
+ * Question Link: https://leetcode.com/problems/remove-element/
+ * Primary idea: keep a index, compare the element at that index with val while moving forward
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ */
+
+class RemoveElement {
+    func removeElement(inout nums: [Int], _ val: Int) -> Int {
+        var lastIndex: Int = 0
+        
+        if nums.count == 0 {
+            return lastIndex
+        }
+        
+        for i in 0...nums.count - 1 {
+            if nums[i] != val {
+                nums[lastIndex] = nums[i]
+                lastIndex += 1
+            }
+        }
+        
+        return lastIndex
+    }
+}

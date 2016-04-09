6b6a0210dcec86de7b9918430c154c69d2c4d638
Yi Gu <yi@superfamily.me>
2016-04-08 19:57:10 -0700

    [Array] add solution to Remove Duplicates From Sorted Array

---
+++ Array/RemoveDuplicatesFromSortedArray.swift
@@ -0,0 +1,25 @@
+/**
+ * Question Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * Primary idea: keep a index, compare the element at that index with the element moving forward
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ */
+
+class RemoveDuplicatesFromSortedArray {
+    func removeDuplicates(inout nums: [Int]) -> Int {
+        if nums.count <= 1 {
+            return nums.count
+        }
+    
+        var lastIndex: Int = 0
+        
+        for i in 1...nums.count - 1 {
+            if nums[i] != nums[lastIndex] {
+                lastIndex += 1
+                nums[lastIndex] = nums[i]
+            }
+        }
+        
+        return lastIndex + 1
+    }
+}

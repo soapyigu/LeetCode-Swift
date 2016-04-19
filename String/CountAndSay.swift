ba4a20a4e8149f79afad9dc716014a2a61423b71
Yi Gu <ro.deliberate@gmail.com>
2016-04-18 21:03:02 -0700

    [String] add solution to Count and Say

---
+++ String/CountAndSay.swift
@@ -0,0 +1,58 @@
+/**
+ * Question Link: https://leetcode.com/problems/count-and-say/
+ * Primary idea: Recursive iteration, use outer iteration to count times, 
+ *               use inner iteration to get the right string for specific index
+ * 
+ * Note: Swift does not have a way to access a character in a string with O(1), 
+ *       thus we have to first transfer the string to a character array
+ * Time Complexity: O(n^2), Space Complexity: O(n)
+ *
+ */
+
+class CountAndSay {
+    func countAndSay(n: Int) -> String {
+        guard n > 0 else {
+            return ""
+        }
+  
+        var res = "1"
+        var temp: String
+        var count: Int
+        var chars: [Character]
+        var current: Character
+  
+        for _ in 1..<n{
+            temp = ""
+            count = 1
+            chars = _strToChars(res)
+            current = chars[0]
+    
+            for i in 1..<res.characters.count {
+                if chars[i] == current {
+                    count += 1
+                } else {
+                    temp.append(Character("\(count)"))
+                    temp.append(current)
+                    count = 1
+                    current = chars[i]
+                }
+            }
+            temp.append(Character("\(count)"))
+            temp.append(current)
+    
+            res = temp
+        }
+  
+        return res
+    }
+
+    private func _strToChars(str: String) -> [Character] {
+        var chars = [Character]()
+        
+        for character in str.characters {
+            chars.append(character)
+        }
+        
+        return chars
+    }
+}

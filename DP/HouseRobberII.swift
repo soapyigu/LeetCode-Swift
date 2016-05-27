a5fc7afa858beb1f3e24f9cf19313558e5b017e7
Yi Gu <ro.deliberate@gmail.com>
2016-05-26 18:32:12 -0700

    [DP] add solutions to House Robber I, II

---
+++ DP/HouseRobberII.swift
@@ -0,0 +1,33 @@
+/**
+ * Question Link: https://leetcode.com/problems/house-robber-ii/
+ * Primary idea: Dynamic Programming, dp[i] = max(dp[i - 1], dp[i - 2], + nums[i])
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ */
+
+class HouseRobberII {
+    func rob(nums: [Int]) -> Int {
+        guard nums.count != 0 else {
+            return 0
+        }
+        guard nums.count != 1 else {
+            return nums[0]
+        }
+    
+        return max(_helper(nums, 0, nums.count - 2), _helper(nums, 1, nums.count - 1))
+    }
+    
+    private func _helper(nums:[Int], _ start: Int, _ end: Int) -> Int {
+        var pre = 0
+        var cur = 0
+        var res = 0
+        
+        for i in start ... end {
+            res = max(pre + nums[i], cur)
+            pre = cur
+            cur = res
+        }
+        
+        return res
+    }
+}

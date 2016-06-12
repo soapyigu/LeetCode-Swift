87d93f7adc94b189b9268d8c650262a572f1ea03
Yi Gu <ro.deliberate@gmail.com>
2016-06-12 11:23:03 -0700

    [Sort] code style fixed

--- Sort/MeetingRooms.swift
+++ Sort/MeetingRooms.swift
@@ -16,13 +16,13 @@
  
 class MeetingRooms {
     func canAttendMeetings(intervals: [Interval]) -> Bool {
-        if intervals.count < 2 {
+        guard intervals.count > 1 else {
             return true
         }
         
         var intervals = intervals.sort({$0.start < $1.start})
         
-        for i in 0...intervals.count - 2 {
+        for i in 0 ..< intervals.count - 1 {
             if intervals[i].end > intervals[i + 1].start {
                 return false
             }

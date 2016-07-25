e9650c4547004887b7e9dcded3b299b67023dd49
Yi Gu <ro.deliberate@gmail.com>
2016-07-24 21:30:19 -0700

    [Math] Fix code style for Super Pow

--- Math/SuperPow.swift
+++ Math/SuperPow.swift
@@ -8,24 +8,25 @@
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 
-class Solution {
-    var base = 1337        
-    func pow(a: Int, _ b: Int) -> Int {
-        var ret = 1
-        for i in 0 ..< b {
-            ret = ret * a % base
-        }
-        return ret
-    }
-
-    func superPowHelper(a: Int, _ b: [Int], _ idx: Int) -> Int {
-        if (idx < 0) {
-            return 1
-        }
-        return pow(superPowHelper(a, b, idx - 1), 10) * pow(a, b[idx]) % base
-    }
-
+class SuperPow {
+    let base = 1337
+    
     func superPow(a: Int, _ b: [Int]) -> Int {
-        return superPowHelper(a, b, b.count - 1)
+      return _superPowHelper(a, b, b.count - 1)
+    }
+
+    func _pow(a: Int, _ b: Int) -> Int {
+      var ret = 1
+      for _ in 0 ..< b {
+        ret = ret * a % base
+      }
+      return ret
+    }
+    
+    func _superPowHelper(a: Int, _ b: [Int], _ idx: Int) -> Int {
+      guard idx >= 0 else {
+        return 1
+      }
+      return _pow(_superPowHelper(a, b, idx - 1), 10) * _pow(a, b[idx]) % base
     }
 }

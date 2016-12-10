ac0417305cc697b31c4f11d9430799ec6c70d8a0
Yi Gu <ro.deliberate@gmail.com>
2016-12-10 12:38:13 -0800

    [String] optimize solution to One Edit Distance

--- String/OneEditDistance.swift
+++ String/OneEditDistance.swift
@@ -5,57 +5,36 @@
  */
 
 class OneEditDistance {
-    func isOneEditDistance(s: String, _ t: String) -> Bool {
-        let sLen = s.characters.count
-        let tLen = t.characters.count
+    func isOneEditDistance(_ s: String, _ t: String) -> Bool {
+        let sChars = Array(s.characters), tChars = Array(t.characters)
+        let sLen = sChars.count, tLen = tChars.count
     
-        guard abs(sLen - tLen) <= 1 else {
+        guard abs(sLen - tLen) <= 1 && s != t else {
             return false
         }
         
-        if sLen > tLen {
-            return _isAddChar(t, s)
-        } else if sLen == tLen {
-            return _isReplaceChar(s, t)
-        } else {
-            return _isAddChar(s, t)
-        }
-    }
-    
-    private func _isAddChar(s: String, _ t: String) -> Bool {
-        let sChars = [Character](s.characters)
-        let tChars = [Character](t.characters)
+        let s1 = sLen < tLen ? sChars : tChars, s2 = sLen < tLen ? tChars : sChars
+        var index1 = 0, index2 = 0, foundDiff = false
         
-        var tIndex = 0
-        var sIndex = 0
-        
-        while sIndex < sChars.count && tIndex < tChars.count {
-            if sChars[sIndex] != tChars[tIndex]  {
-                tIndex += 1
+        while index1 < s1.count && index2 < s2.count {
+            if s1[index1] != s2[index2] {
+                if foundDiff {
+                    return false
+                }
+                foundDiff = true
+                
+                if s1.count < s2.count {
+                    index2 += 1
+                } else {
+                    index1 += 1
+                    index2 += 1
+                }
             } else {
-                tIndex += 1
-                sIndex += 1
+                index1 += 1
+                index2 += 1
             }
         }
         
-        return tIndex - sIndex <= 1
-    }
-    
-    private func _isReplaceChar(s: String, _ t: String) -> Bool {
-        let sChars = [Character](s.characters)
-        let tChars = [Character](t.characters)
-        
-        var tIndex = 0
-        var diff = 0
-        
-        for sIndex in 0 ..< sChars.count {
-            if sChars[sIndex] != tChars[tIndex] {
-                diff += 1
-            }
-            
-            tIndex += 1
-        }
-        
-        return diff == 1
+        return true
     }
 }

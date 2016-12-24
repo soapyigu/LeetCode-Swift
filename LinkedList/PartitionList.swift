864f7031b962ba9279a1e8dee8343fa3a6e7367e
Yi Gu <ro.deliberate@gmail.com>
2016-12-24 12:19:27 -0800

    [LinkedList] Optimize Solutions

--- LinkedList/PartitionList.swift
+++ LinkedList/PartitionList.swift
@@ -1,8 +1,6 @@
 /**
  * Question Link: https://leetcode.com/problems/partition-list/
- * Primary idea: Tail Insert and merge two lists
- *
- * Note: Swift provides "!==" to determine two objects refer to the same reference
+ * Primary idea: Tail Insert and merge two lists, use dummy to avoid edge case
  * 
  * Time Complexity: O(n), Space Complexity: O(1)
  * Definition for singly-linked list.
@@ -17,23 +15,20 @@
  */
 
 class PartitionList {
-    func partition(head: ListNode?, _ x: Int) -> ListNode? {
-        let prevDummy = ListNode(0)
-        var prev = prevDummy
-        let postDummy = ListNode(0)
-        var post = postDummy
+    func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        let prevDummy = ListNode(0), postDummy = ListNode(0)
+        var prev = prevDummy, post = postDummy
         
         var node = head
         
         while node != nil {
             if node!.val < x {
                 prev.next = node
-                prev = node!
+                prev = prev.next!
             } else {
                 post.next = node
-                post = node!
+                post = post.next!
             }
-            
             node = node!.next
         }
         

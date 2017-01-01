16270f61cf0181cb8db32e6ebf3b7f008393a0d8
Yi Gu <ro.deliberate@gmail.com>
2017-01-01 10:30:12 -0800

    [DP] Optimize code style for Decode Ways

--- DP/DecodeWays.swift
+++ DP/DecodeWays.swift
@@ -8,25 +8,19 @@
 
 class DecodeWays {
     func numDecodings(_ s: String) -> Int {
-        let chars = Array(s.characters), len = chars.count
+        let sChars = Array(s.characters), len = sChars.count
+        var dp = Array(repeating: 0, count: len + 1)
+        dp[0] = 1
         
-        guard len > 0 else {
+        guard len >= 1 else {
             return 0
         }
         
-        var dp = Array(repeating: 0, count: len + 1)
-        dp[0] = 1
-        dp[1] = isValid(String(chars[0 ..< 1])) ? 1 : 0
-       
-        guard len >= 2 else {
-            return dp[len]
-        }
-        
-        for i in 2 ... len {
-            if isValid(String(chars[i - 1 ..< i])) {
+        for i in 1 ... len {
+            if isValid(String(sChars[i - 1 ..< i])) {
                 dp[i] += dp[i - 1]
             }
-            if isValid(String(chars[i - 2 ..< i])) {
+            if i >= 2 && isValid(String(sChars[i - 2 ..< i])) {
                 dp[i] += dp[i - 2]
             }
         }
@@ -34,15 +28,15 @@ class DecodeWays {
         return dp[len]
     }
     
-    private func isValid(_ str: String) -> Bool {
-        let chars = Array(str.characters)
-        
-        if chars[0] == "0" {
+    private func isValid(_ numStr: String) -> Bool {
+        if Array(numStr.characters).first == "0" {
+            return false
+        }
+    
+        guard let num = Int(numStr) else {
             return false
         }
         
-        let num = Int(str)!
-        
-        return num > 0 && num <= 26
+        return num >= 1 && num <= 26
     }
 }

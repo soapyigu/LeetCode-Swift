ba5badd4d4c6f0f0e35baa0819ebfc9ca5c6cc33
Yi Gu <ro.deliberate@gmail.com>
2017-03-18 22:12:54 -0700

    Add Solution to FizzBuzz

---
+++ String/FizzBuzz.swift
@@ -0,0 +1,31 @@
+/**
+ * Question Link: https://leetcode.com/problems/fizz-buzz/
+ * Primary idea: Iterate the array and handle multiples of 3 or 5 separately.
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ */
+
+class FizzBuzz {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var res = [String]()
+    
+        if n < 0 {
+            return res
+        }
+        
+        for i in 1 ... n {
+            if i % 3 == 0 && i % 5 == 0 {
+                res.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                res.append("Fizz")
+            } else if i % 5 == 0 {
+                res.append("Buzz")
+            } else {
+                res.append("\(i)")
+            }
+        }
+        
+        return res
+    }
+}

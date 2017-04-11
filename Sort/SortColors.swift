4ec4f29592ccf4236e2ef568a419ab53d6fb6536
Yi Gu <rio.deliberate@gmail.com>
2017-04-10 22:27:45 -0700

    Optimize solution to Sort Colors

--- Sort/SortColors.swift
+++ Sort/SortColors.swift
@@ -6,13 +6,7 @@
 
 class SortColors {
     func sortColors(_ nums: inout [Int]) {
-        guard nums.count > 1 else {
-            return
-        }
-        
-        var red = 0
-        var blue = nums.count - 1
-        var i = 0
+        var red = 0, blue = nums.count - 1, i = 0
         
         while i <= blue {
             if nums[i] == 0 {
@@ -28,7 +22,7 @@ class SortColors {
         }
     }
     
-    private func _swap<T>(_ nums: inout [T], _ p: Int, _ q: Int) {
+    fileprivate func _swap<T>(_ nums: inout [T], _ p: Int, _ q: Int) {
         (nums[p], nums[q]) = (nums[q], nums[p])
     }
 }

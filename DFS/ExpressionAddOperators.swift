f9a0077d69ad04c54374d35e9cbc25afe0e94bdc
Yi Gu <rio.deliberate@gmail.com>
2017-10-01 22:55:56 -0700

    [DFS] Update Expression Add Operators with Swift 4

--- DFS/ExpressionAddOperators.swift
+++ DFS/ExpressionAddOperators.swift
@@ -14,37 +14,56 @@
 class ExpressionAddOperators {
     func addOperators(_ num: String, _ target: Int) -> [String] {
         var res = [String]()
-        let numChars = Array(num.characters)
-        
-        guard numChars.count > 0 else {
+
+        guard num.count > 0 else {
             return res
         }
-        
-        dfs(&res, "", numChars, target, 0, 0, 0)
-        
+
+        dfs(&res, "", num, target, 0, 0, 0)
+
         return res
     }
-    
-    private func dfs(_ res: inout [String], _ temp: String, _ numChars: [Character], _ target: Int, _ pos: Int, _ eval: Int, _ mul: Int) {
-        if pos == numChars.count {
+
+    private func dfs(_ res: inout [String], _ temp: String, _ num: String, _ target: Int, _ pos: Int, _ eval: Int, _ mul: Int) {
+        if pos == num.count {
             if eval == target {
                 res.append(temp)
             }
             return
         }
-        
-        for i in pos..<numChars.count {
-            if i != pos && numChars[pos] == "0" {
+
+        for i in pos..<num.count {
+            if i != pos && num[i] == "0" {
                 break
             }
-            let curt = Int(String(numChars[pos..<i + 1]))!
+            let curt = Int(num[pos..<i + 1])!
             if pos == 0 {
-                dfs(&res, temp + String(curt), numChars, target, i + 1, curt, curt)
+                dfs(&res, temp + String(curt), num, target, i + 1, curt, curt)
             } else {
-                dfs(&res, temp + "+" + String(curt), numChars, target, i + 1, eval + curt, curt)
-                dfs(&res, temp + "-" + String(curt), numChars, target, i + 1, eval - curt, -curt)
-                dfs(&res, temp + "*" + String(curt), numChars, target, i + 1, eval - mul + mul * curt, mul * curt)
+                dfs(&res, temp + "+" + String(curt), num, target, i + 1, eval + curt, curt)
+                dfs(&res, temp + "-" + String(curt), num, target, i + 1, eval - curt, -curt)
+                dfs(&res, temp + "*" + String(curt), num, target, i + 1, eval - mul + mul * curt, mul * curt)
             }
         }
     }
+}
+
+extension String {
+  subscript(index: Int) -> String {
+    get {
+      assert(index < self.count)
+      return String(Array(self.characters)[index])
+    }
+  }
+  
+  subscript(range: CountableRange<Int>) -> String {
+    get {
+      var result = ""
+      for i in range {
+        assert(i < self.count)
+        result.append(self[i])
+      }
+      return result
+    }
+  }
 }

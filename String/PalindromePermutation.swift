c3799cf061c7516d5b7a208737ea6967ca3db857
Yi Gu <rio.deliberate@gmail.com>
2018-01-05 00:03:54 -0800

    Update Palindrome Permutation Solution to a more Swift way

--- String/PalindromePermutation.swift
+++ String/PalindromePermutation.swift
@@ -5,29 +5,23 @@
  */
 
 class PalindromePermutation {
-    func canPermutePalindrome(s: String) -> Bool {
-        let sChars = [Character](s.characters)
-        var oddNum = 0
-        var charFrequency = [Character: Int]()
-        
-        for char in sChars {
-            if charFrequency[char] != nil {
-                charFrequency[char]! += 1
-            } else {
-                charFrequency[char] = 1
-            }
-        }
-        
-        for char in charFrequency.keys {
-            let fre = charFrequency[char]!
-            if fre % 2 == 1 {
-                oddNum += 1
-            }
-            if oddNum >= 2 {
-                return false
-            }
-        }
-        
-        return true
+  func canPermutePalindrome(_ s: String) -> Bool {
+    var oddNum = 0
+    
+    for (_, value) in s.frequencies where value % 2 == 1 {
+      oddNum += 1
+      
+      if oddNum >= 2 {
+        return false
+      }
     }
+    
+    return true
+  }
+}
+
+extension Sequence where Element: Hashable {
+  var frequencies: [Element: Int] {
+    return Dictionary(self.map{ ($0, 1)}, uniquingKeysWith: +)
+  }
 }

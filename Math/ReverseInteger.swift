60e7b01b9ef2ec648b5a2d918db52937ebe12833
maligh <455080311@qq.com>
2018-04-03 00:57:13 +0800

    Fix

--- Math/ReverseInteger.swift
+++ Math/ReverseInteger.swift
@@ -7,19 +7,16 @@
  */
  
  class ReverseInteger {
-    func reverse(x: Int) -> Int {
+    func reverse(_ x: Int) -> Int {
         var res = 0
         var x = x
-  
         while x != 0 {
-            res = res * 10 + x % 10
-            x = x / 10
-            
-            if res > Int(Int32.max) || res < Int(Int32.min) {
+            if res > Int(Int32.max) / 10 || res < Int(Int32.min) / 10 {
                 return 0
             }
+            res = res * 10 + x % 10
+            x = x / 10
         }
-        
         return res
     }
 }

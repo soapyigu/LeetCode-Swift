ef70494fd57801c8884ae2e03d4ddfde1f865ad4
Yi Gu <rio.deliberate@gmail.com>
2018-04-04 07:22:43 +0800

    [Array] Update solution to Longest Consecutive Sequence

--- Array/LongestConsecutiveSequence.swift
+++ Array/LongestConsecutiveSequence.swift
@@ -8,22 +8,21 @@
 
 class LongestConsecutiveSequence {
     func longestConsecutive(_ nums: [Int]) -> Int {
-        var set = Set(nums)
-        var longest = 0
+        var set = Set(nums), longest = 0
         
         for num in nums {
             if set.contains(num) {
                 set.remove(num)
-                longest = max(1 + findConsecutive(&set, num, 1) + findConsecutive(&set, num, -1), longest)
+                let distance = 1 + findConsecutive(&set, num, 1) + findConsecutive(&set, num, -1)
+                longest = max(longest, distance)
             }
         }
         
         return longest
     }
     
-    private func findConsecutive(_ set: inout Set<Int>, _ num: Int, _ step: Int) -> Int {
-        var len = 0
-        var num = num + step
+    fileprivate func findConsecutive(_ set: inout Set<Int>, _ num: Int, _ step: Int) -> Int {
+        var len = 0, num = num + step
     
         while set.contains(num) {
             set.remove(num)

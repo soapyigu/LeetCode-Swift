5699e903d1b447914e0051584c2823d698e8cea6
Yi Gu <rio.deliberate@gmail.com>
2018-07-10 20:21:08 -0700

    [String] Update solution to Text Justification

--- String/TextJustification.swift
+++ String/TextJustification.swift
@@ -8,72 +8,71 @@
 class TextJustification {
     func fullJustify(_ words: [String], _ maxWidth: Int) -> [String] {
         var res = [String]()
-        var count = 0, last = 0
+        var last = 0, currentLineLength = 0
         
-        guard words.count > 0 && maxWidth >= 0 else {
-            return res
-        }
-        
-        for i in 0..<words.count {
-            let wordLen = words[i].count
-        
-            if count + (i - last) + wordLen > maxWidth {
-                var spaceNum = 0
-                var extraNum = 0
+        for (i, word) in words.enumerated() {
+            if currentLineLength + word.count + (i - last) > maxWidth {
                 
-                if i - last - 1 > 0 {
-                    spaceNum = (maxWidth - count) / (i - last - 1)
-                    extraNum = (maxWidth - count) % (i - last - 1)
-                    res.append(buildLine(words, last, i - 1, spaceNum, extraNum))
-                } else {
-                    res.append(buildSpecialLine(words, last, last, maxWidth))
-                }
-                count = 0
+                res.append(buildLine(words, last, i - 1, maxWidth, currentLineLength))
+                
                 last = i
+                currentLineLength = 0   
             }
             
-            count += wordLen
-        } 
+            currentLineLength += word.count
+        }
         
-        res.append(buildSpecialLine(words, last, words.count - 1, maxWidth))
+        res.append(buildLastLine(words, last, words.count - 1, maxWidth))
         
         return res
     }
     
-    fileprivate func buildLine(_ words: [String], _ start: Int, _ end: Int, _ spaceNum: Int, _ extraNum: Int) -> String {
-        var res = ""
-        var extraNum = extraNum
+    fileprivate func buildLine(_ words: [String], _ start: Int, _ end: Int, _ maxWidth: Int, _ currentLineLength: Int) -> String {
+        var line = ""
+        var extraSpaceNum = 0, spaceNum = 0
+        
+        if end > start {
+            extraSpaceNum = (maxWidth - currentLineLength) % (end - start)
+            spaceNum = (maxWidth - currentLineLength) / (end - start)
+        } else {
+            spaceNum = maxWidth - currentLineLength
+        }
         
         for i in start...end {
-            res += words[i]
-            if i != end {
-                for _ in 1...spaceNum {
-                    res += " "
-                }
-                if extraNum != 0 {
-                    res += " "
-                    extraNum -= 1
-                }
+            line.append(words[i])
+            
+            if start != end && i == end {
+                break
+            } 
+            
+            for _ in 0..<spaceNum {
+                line.append(" ")
+            }
+            
+            if extraSpaceNum > 0 {
+                line.append(" ")
+                extraSpaceNum -= 1
             }
         }
         
-        return res
+        return line
     }
     
-    fileprivate func buildSpecialLine(_ words: [String], _ start: Int, _ end: Int, _ lineLength: Int) -> String {
-        var res = ""
+    fileprivate func buildLastLine(_ words: [String], _ start: Int, _ end: Int, _ maxWidth: Int) -> String {
+        var line = ""
         
         for i in start...end {
-            res += words[i]
+            line.append(words[i])
+            
             if i < end {
-                res += " "
+                line.append(" ")
             }
         }
         
-        for _ in res.count..<lineLength {
-            res += " "
+        while line.count < maxWidth {
+            line.append(" ")
         }
         
-        return res
+        return line
     }
 }

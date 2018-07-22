0e5923b09da2eef5fe92b09e974fef9007e6ac98
Yi Gu <rio.deliberate@gmail.com>
2018-07-22 16:02:55 -0700

    [Math] Optimize solution to Add Two Numbers

--- Math/AddTwoNumbers.swift
+++ Math/AddTwoNumbers.swift
@@ -16,29 +16,26 @@
 
 class AddTwoNumbers {
     func addTwoNumbers(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var carry = 0, sum = 0, l1 = l1, l2 = l2
+        var carry = 0, l1 = l1, l2 = l2
         let dummy = ListNode(0)
         var node = dummy
         
         while l1 != nil || l2 != nil || carry != 0 {
-            sum = carry
-            
             if l1 != nil {
-                sum += l1!.val
+                carry += l1!.val
                 l1 = l1!.next
             }
             if l2 != nil {
-                sum += l2!.val
+                carry += l2!.val
                 l2 = l2!.next
             }
             
-            carry = sum / 10
-            
-            node.next = ListNode(sum % 10)
+            node.next = ListNode(carry % 10)
             node = node.next!
+            
+            carry = carry / 10
         }
         
-        return dummy.next
-        
+        return dummy.next 
     }
 }

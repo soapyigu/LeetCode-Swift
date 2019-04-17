c46f4f7fd28a5e15626335b2c0cb2ba3ab590943
Zhou Fang <wamaker.scorpion@gmail.com>
2019-04-17 11:23:44 +0800

    remove comment

--- LinkedList/LFUCache.swift
+++ LinkedList/LFUCache.swift
@@ -136,10 +136,3 @@ class CacheNode {
 		self.val = val
 	}
 }
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * let obj = LFUCache(capacity)
- * let ret_1: Int = obj.get(key)
- * obj.put(key, value)
- */

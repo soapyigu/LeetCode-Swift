65ab623eee59a997e32f13d15be005eac12ae07d
h.crane <h.crane.t@gmail.com>
2019-09-25 03:16:31 +0900

    append result

---
+++ Sort/KthLargestElementInAStream.swift
@@ -0,0 +1,22 @@
+final class KthLargest {
+    
+    private let k: Int
+    private var nums: [Int]
+
+    init(_ k: Int, _ nums: [Int]) {
+        self.k = k
+        self.nums = nums.sorted { $1 < $0 }
+    }
+    
+    func add(_ val: Int) -> Int {
+        self.nums.append(val)
+        
+        for i in 0...nums.count-1 {
+            if nums[i] < val {
+                nums.insert(val, at: i)
+                break
+            }
+        }
+        return self.nums[self.k-1]
+    }
+}

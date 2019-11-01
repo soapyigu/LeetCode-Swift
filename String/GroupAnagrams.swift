2b79ccd2e6e4690d3aa45f0df6b972e628f40352
Soap <soapyigu@users.noreply.github.com>
2019-10-31 19:54:14 -0700

    [String] Refactor solution to Group Anagrams

--- String/GroupAnagrams.swift
+++ String/GroupAnagrams.swift
@@ -2,25 +2,20 @@
  * Question Link: https://leetcode.com/problems/anagrams/
  * Primary idea: Iterate the string array and categories strings with the same sorted one
  *
- * Time Complexity: O(nmlogm + nlogn), n stands for number of words, m stands for the length of a word
- *                  Space Complexity: O(n)
+ * Time Complexity: O(nmlogm), n stands for number of words, m stands for the length of a word
+ * Space Complexity: O(n)
  */
 
 class GroupAnagrams {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        var map = [String: [String]]()
+        var sortedStrToStrs = [String: [String]]()
   
         for str in strs {
             let sortedStr = String(str.sorted())
-    
-            var anagrams = [String]()
-            if let list = map[sortedStr] {
-                anagrams = list
-            }
-            anagrams.append(str)
-            map[sortedStr] = anagrams
+            
+            sortedStrToStrs[sortedStr, default: []].append(str)
         }
-  
-        return map.values.map { value in value.sorted() }
+        
+        return Array(sortedStrToStrs.values)
     }
 }

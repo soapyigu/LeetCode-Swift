a9aff18eb2bb3cdb9c4e00b3da92a744adfb94ce
Alex Xie <xieweialex@gmail.com>
2019-11-04 07:51:15 -0800

    [Search] Improved solution for 'search insert position'

--- Search/SearchInsertPosition.swift
+++ Search/SearchInsertPosition.swift
@@ -35,4 +35,32 @@ class SearchInsertPosition {
         
         return right
     }
+
+    func searchInsertI(nums: [Int], _ target: Int) -> Int {
+        guard nums.count > 0 else {
+            return 0
+        }
+        
+        var left = 0
+        var right = nums.count - 1
+
+        var res = 0
+        while left <= right {
+            let mid = left + (right - left) / 2 
+            if nums[mid] == target {
+
+                return mid
+            } else if nums[mid] < target {
+                
+                res = mid + 1
+                left = mid + 1
+            } else {
+
+                res = mid 
+                right = mid - 1
+            }
+        }
+        
+        return res
+    }
 }

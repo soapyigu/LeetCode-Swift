b0cfeeebeec0c87fa2ff754b6dcd276acda7b55b
Yi Gu <rio.deliberate@gmail.com>
2019-12-02 16:26:35 -0800

    Refactor solutions to Valid Sudoku and Serialize and Deserialize Binary Tree

--- Tree/SerializeDeserializeBinaryTree.swift
+++ Tree/SerializeDeserializeBinaryTree.swift
@@ -1,6 +1,6 @@
 /**
  * Question Link: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
- * Primary idea: Preorder
+ * Primary idea: Preorder or level order
  * Time Complexity: O(n), Space Complexity: O(n)
  * 
  * Definition for a binary tree node.
@@ -16,7 +16,8 @@
  * }
  */
 
- class SerializeDeserializeBinaryTree {
+# Solution 1 - Preorder
+class SerializeDeserializeBinaryTree {
     func serialize(_ root: Node?) -> String {
         guard let root = root else {
             return "#"
@@ -33,6 +34,69 @@
         let root = Node(rootVal)
         root.left = deserialize(&vals)
         root.right = deserialize(&vals)
+        
+        return root
+    }
+}
+
+# Solution 2 - Level order, BFS
+class SerializeDeserializeBinaryTree {
+    func serialize(_ root: Node?) -> String {
+        guard let root = root else {
+            return ""
+        }
+        
+        var res = "", nodes: [Node?] = [root]
+        
+        while !nodes.isEmpty {
+            let currentLevelSize = nodes.count
+            
+            for _ in 0..<currentLevelSize {
+                let node = nodes.removeFirst()
+                
+                if let node = node {
+                    res.append(String(node.val))
+                    
+                    nodes.append(node.left)
+                    nodes.append(node.right)
+                } else {
+                    res.append("#")
+                }
+            }
+        }
+        
+        return res
+    }
+    
+    func deserialize(_ vals: String) -> Node? {
+        guard let firstVal = vals.first, let rootVal = Int(String(firstVal)) else {
+            return nil
+        }
+        
+        let root = Node(rootVal), vals = Array(vals)
+        var nodes: [Node?] = [root], i = 1
+        
+        while !nodes.isEmpty {
+            guard let node = nodes.removeFirst() else {
+                continue
+            }
+            
+            var left: Node?
+            if let leftVal = Int(String(vals[i])) {
+                left = Node(leftVal)
+            }
+            node.left = left
+            nodes.append(left)
+            i += 1
+            
+            var right: Node?
+            if let rightVal = Int(String(vals[i])) {
+                right = Node(rightVal)
+            }
+            node.right = right
+            nodes.append(right)
+            i += 1
+        }
         
         return root
     }

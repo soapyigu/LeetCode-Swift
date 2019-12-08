2b1e12c33c1d3b8077253a7f6d9dfa926820974a
Binh An Tran <tran.binhan@gmail.com>
2019-12-08 14:19:17 +0100

    Fix ValidPalindrome

--- String/ValidPalindrome.swift
+++ String/ValidPalindrome.swift
@@ -10,16 +10,16 @@ class ValidPalindrome {
     func isPalindrome(_ s: String) -> Bool {
         var i = 0, j = s.count - 1
         let sChars = Array(s.lowercased())
-        
+
         while i < j {
             while !sChars[i].isAlphanumeric && i < j {
                 i += 1
             }
-            
+
             while !sChars[j].isAlphanumeric && i < j {
                 j -= 1
             }
-            
+
             if sChars[i] != sChars[j] {
                 return false
             } else {
@@ -27,13 +27,13 @@ class ValidPalindrome {
                 j -= 1
             }
         }
-        
+
         return true
     }
 }
 
 extension Character {
-    var isValid: Bool {
+    var isAlphanumeric: Bool {
         return isLetter || isNumber
     }
 }

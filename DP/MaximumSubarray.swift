f1552ebcc8803c3d2542cd1495b313fd03eb5aab
Jayven Nhan <nhantrivinh@gmail.com>
2019-12-28 22:28:02 +0700

    Update Swift naming convention in accordance with latest style guide and simplify value reference in for-loop

--- DP/MaximumSubarray.swift
+++ DP/MaximumSubarray.swift
@@ -6,15 +6,14 @@
  */
 
 class MaximumSubarray {
-    func maxSubArray(nums: [Int]) -> Int {
-        var max_current = nums[0]
-        var max_global = nums[0]
-        
-        for i in 1..<nums.count {
-            max_current = max(max_current + nums[i], nums[i])
-            max_global = max(max_current, max_global)
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var nums = nums
+        var ret = nums.removeFirst()
+        var curr = ret
+        for num in nums {
+            curr = max(num, curr + num)
+            ret = max(ret, curr)
         }
-        
-        return max_global
+        return ret
     }
 }

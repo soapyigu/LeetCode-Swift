cd52319974991f737cbcbeb268c853906558aee9
Yi Gu <yigucmu@gmail.com>
2022-08-14 21:42:19 -0700

    Refactor code style to Permutations II

--- DFS/PermutationsII.swift
+++ DFS/PermutationsII.swift
@@ -10,29 +10,26 @@ class PermutationsII {
     func permuteUnique(_ nums: [Int]) -> [[Int]] {
         var res = [[Int]](), path = [Int](), visited = [Bool](repeating: false, count: nums.count)
         
-        let nums = nums.sorted(by: <)
-        
-        _dfs(&res, &path, nums, &visited)
+        dfs(&res, &path, &visited, nums.sorted())
         
         return res
     }
     
-    private func _dfs(inout res: [[Int]], inout _ path: [Int], _ nums: [Int], inout _ visited: [Bool]) {
-        // termination case
+    private func dfs(_ res: inout [[Int]], _ path: inout [Int], _ isVisited: inout [Bool], _ nums: [Int]) {
         if path.count == nums.count {
             res.append(path)
             return
         }
         
-        for i in 0..<nums.count {
-            if visited[i] || (i > 0 && nums[i] == nums[i - 1] && visited[i - 1]) {
-                continue
+        for (i, num) in nums.enumerated() where !isVisited[i] {
+            if i > 0 && nums[i] == nums[i - 1] && !isVisited[i - 1] {
+                continue   
             }
             
-            path.append(nums[i])
-            visited[i] = true
-            _dfs(&res, &path, nums, &visited)
-            visited[i] = false
+            path.append(num)
+            isVisited[i] = true
+            dfs(&res, &path, &isVisited, nums)
+            isVisited[i] = false
             path.removeLast()
         }
     }

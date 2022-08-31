376a0d3ff9cb88c34578f7742ebf7b36ea81fde1
Sorokin Igor <34890417+srk1nn@users.noreply.github.com>
2022-08-31 12:31:40 +0300

    [Search] fix code style

--- Search/GuessNumberHigherOrLower.swift
+++ Search/GuessNumberHigherOrLower.swift
@@ -5,7 +5,7 @@
  *
  * Forward declaration of guess API.
  * @param  num -> your guess number
- * @return    -1 if num is higher than the picked number
+ * @return -1 if num is higher than the picked number
  *         1 if num is lower than the picked number
  *         otherwise return 0
  * func guess(_ num: Int) -> Int

26c5865c4fbb8866312975a24a3b776ffebf814c
Soap <soapyigu@users.noreply.github.com>
2023-11-04 11:22:00 -0700

    Refactor solution to the Valid Palindrome II

--- String/ValidPalindromeII.swift
+++ String/ValidPalindromeII.swift
@@ -1,38 +1,40 @@
 /**
  * Question Link: https://leetcode.com/problems/valid-palindrome-ii/
- * Primary idea: Take advantage of validPalindrome, and jump left and right separately to get correct character should be deleted
+ * Primary idea: Two pointers. Move left and right when they are equal or cannot separate by moving either side, otherwise move one direction and update the flag.
  *
- * Time Complexity: O(n), Space Complexity: O(n)
+ * Time Complexity: O(n), Space Complexity: O(1)
  *
  */
 
 class ValidPalindromeII {
     func validPalindrome(_ s: String) -> Bool {
+        var i = 0, j = s.count - 1, isDeleted = false
         let s = Array(s)
-        return isValid(true, s) || isValid(false, s)
-    }
-    
-    private func isValid(_ skipLeft: Bool, _ s: [Character]) -> Bool {
-        var i = 0, j = s.count - 1, alreadySkipped = false
-        
+
         while i < j {
-            if s[i] == s[j] {
-                i += 1
-                j -= 1
-            } else {
-                if alreadySkipped {
+            if s[i] != s[j] {
+                if isDeleted {
                     return false
                 } else {
-                    alreadySkipped = true
-                    if skipLeft {
-                        i += 1 
-                    } else {
+                    if s[i + 1] == s [j] && s[j - 1] == s[i] {
+                        i += 1
                         j -= 1
+                    } else if s[i + 1] == s[j] {
+                        i += 1
+                        isDeleted = true
+                    } else if s[j - 1] == s[i] {
+                        j -= 1
+                        isDeleted = true
+                    } else {
+                        return false
                     }
                 }
+            } else {
+                i += 1
+                j -= 1
             }
         }
-        
+
         return true
     }
 }
